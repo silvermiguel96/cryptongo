cc2e1de6e43f467d413eee0ca627a3c6016ff041
Miguelangel <silvermiguel96@gmail.com>
2018-10-23 14:24:56 -0500

    Apis

--- api/main.py
+++ api/main.py
@@ -18,7 +18,7 @@ def get_documents(): #Obtener todo los documentos de nuestra base de datos.
     limit = int(request.args.get('limit', 0)) #Variable limit mongo
     if name :
         params.update({ 'name': name})
-    cursor = db_connection.tickers.find(
+    cursor = db_connection.ticker.find(
         params, {'_id': 0, 'ticker_hash':0}
     ).limit(limit) # variable que traemos de mongo
     return list(cursor) #Lista nombre funcion de python
@@ -29,9 +29,9 @@ def get_top20(): #Permite traer el top20 de las monedas
     limit = int(request.args.get('limit', 0)) #Variable limit mongo
     if name :
         params.update({ 'name': name})
-        params.update({ 'rank': {'$lte': 20}}) # Los primeros 20
-    cursor = db_connection.tickers.find(
-        params, {'_id': 0, 'ticker_hash':0}
+    params.update({ 'rank': {'$lte': 20}}) # Los primeros 20
+    cursor = db_connection.ticker.find(
+        params, {'_id': 0, 'ticker_hash': 0}
     ).limit(limit) # variable que traemos de mongo
     return list(cursor) #Lista nombre funcion de python
 
@@ -46,3 +46,16 @@ def remove_currency(): # Remover un tiques de una moneda
         params
     ).deleted_count
     
+# Ruta inicial
+@app.route("/")
+def index():
+     return jsonify(
+         {
+             'name' : 'Cryptongo API'
+         }
+     )
+@app.route("/top20", methods=['GET']) #EXPLICAR el metodo que vamos a utilizar
+def top20():
+    return jsonify(
+        get_top20()
+    )

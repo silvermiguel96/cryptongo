199cfeb6e929fa5b44894ca6f0673381febc63c8
Miguelangel <silvermiguel96@gmail.com>
2018-10-23 12:45:29 -0500

    Creando Romove y top20

--- api/main.py
+++ api/main.py
@@ -24,7 +24,25 @@ def get_documents(): #Obtener todo los documentos de nuestra base de datos.
     return list(cursor) #Lista nombre funcion de python
  
 def get_top20(): #Permite traer el top20 de las monedas
-    pass
+    params = {}
+    name = request.args.get('name', '') # Variable de la base de datos nombre
+    limit = int(request.args.get('limit', 0)) #Variable limit mongo
+    if name :
+        params.update({ 'name': name})
+        params.update({ 'rank': {'$lte': 20}}) # Los primeros 20
+    cursor = db_connection.tickers.find(
+        params, {'_id': 0, 'ticker_hash':0}
+    ).limit(limit) # variable que traemos de mongo
+    return list(cursor) #Lista nombre funcion de python
 
 def remove_currency(): # Remover un tiques de una moneda
-    pass
+    params = {}
+    name = request.args.get('name', '')
+    if name:
+        params.update({'name': name})
+    else:
+        return False
+    return db_connection.tickers.delete_many(
+        params
+    ).deleted_count
+    
